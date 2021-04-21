4003af32f9b6c9e2a0956f8a71c39eb9d0803532
Светлана <Totik0491@yandex.ru>
2021-04-21 15:19:38 +0300

    Задания

--- PracticalWork5/Program.cs
+++ PracticalWork5/Program.cs
@@ -6,10 +6,16 @@ namespace PracticalWork5
     {
         static void Main(string[] args)
         {
-            Folder.ClassDocument.SaveDateNow();
+            Console.WriteLine("-------------------Задание 1-------------------");
             Folder.ClassDocument.SaveInDocument();
+            Console.WriteLine();
+            Console.WriteLine("-------------------Задание 2-------------------");
+            Folder.ClassDocument.SaveDateNow();
+            Console.WriteLine();
+            Console.WriteLine("-------------------Задание 3-------------------");
             Folder.ClassDocument.BinaryFile();
-
+            Console.WriteLine();
+            Console.WriteLine("-------------------Задание 4-------------------");
             Folder.ClassEmployee[] persArray = new Folder.ClassEmployee[5];
             persArray[0] = new Folder.ClassEmployee("Егоров Александр Михайлович", "Директор", "eaM@mail.ru", "+7-985-963-52-85", 250000, 65);
             persArray[1] = new Folder.ClassEmployee("Беспалый Георгий Валерьевич", "Менеджер", "bgV@mail.ru", "+7-852-963-41-52", 80000, 45);
@@ -17,7 +23,7 @@ namespace PracticalWork5
             persArray[3] = new Folder.ClassEmployee("Симановская Ирина Александровна", "Дизайнер", "siA@mail.ru", "+7-985-745-65-45", 135000, 25);
             persArray[4] = new Folder.ClassEmployee("Алинов Дмитрий Евгеньевич", "Главный бухгалтер", "adE@mail.ru", "+7-235-654-85-25", 220000, 42);
 
-
+            
             for (int i = 0; i < persArray.Length; i++)
                 if (persArray[i].age > 40)
                 {
